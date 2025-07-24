c4d49b2959287a675068c792c790ae477321faa8
Njarah123 <hasinjararandriambalo@gmai.com>
2025-07-24 07:04:28 +0300

    Disable custom Flyway config in production

--- src/main/java/com/library/config/FlywayConfig.java
+++ src/main/java/com/library/config/FlywayConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 @Configuration
@@ -17,6 +18,7 @@ public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
     @DependsOn("dataSource")
+    @Profile("!prod") // Désactiver en production
     public Flyway flyway(DataSource dataSource) {
         Flyway flyway = Flyway.configure()
                 .dataSource(dataSource)
