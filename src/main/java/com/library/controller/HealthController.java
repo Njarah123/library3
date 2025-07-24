489cf8608ff005394b13c129f0e0ff3739e5ca79
Njarah123 <hasinjararandriambalo@gmai.com>
2025-07-24 08:39:03 +0300

    Fix controller mapping conflict - final deployment ready

--- src/main/java/com/library/controller/HealthController.java
+++ src/main/java/com/library/controller/HealthController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/status")
+    public String status() {
         return "Library Management System is running!";
     }
     
